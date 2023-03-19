4a77806f7b74ff01d61d52c0636987bb66da01f1
Emmanuel Knafo <Emmanuel.Knafo@cad4devops.com>
2023-03-19 13:11:51 -0400

    sequence gets triggered

--- setup.ps1
+++ setup.ps1
@@ -11,9 +11,9 @@ $API_TOKEN = kubectl get secret keptn-api-token -n keptn -ojsonpath='{.data.kept
 Write-Host $API_TOKEN
 keptn auth --endpoint=http://192.168.102.38/api --api-token=$API_TOKEN
 
-$GITHUB_TOKEN = "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"
+$GITHUB_TOKEN = "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"
 $GIT_USER = "emmanuelknafodevopsabcs"
-$GIT_NEW_REPO_NAME = "myway5"
+$GIT_NEW_REPO_NAME = "myway7" #create manually
 
 $GIT_REPO = "https://github.com/$GIT_USER/$GIT_NEW_REPO_NAME.git"
 
@@ -26,6 +26,13 @@ echo "Git Username: $GIT_USER"
 echo "Git Token: $GITHUB_TOKEN"
 echo "New Git repo to be created: $GIT_REPO"
 
-gh repo create $GIT_REPO --public
+#gh repo create $GIT_REPO --public
 keptn create project jes-example -y -s jes-hello-world/shipyard.yaml --git-user=$GIT_USER --git-token=$GITHUB_TOKEN --git-remote-url=$GIT_REPO
-#keptn create service hello --project jes-example -y
+keptn create service hello --project jes-example -y
+
+#Add a simple "Hello world!" job config to production stage for service hello as job/config.yaml
+
+keptn add-resource --project jes-example --service hello --stage production --resource jes-hello-world/jobconfig.yaml --resourceUri job/config.yaml
+#Trigger the example-seq sequence.
+
+keptn trigger sequence example-seq --project jes-example --service hello --stage production
